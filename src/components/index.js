cb16371ffb1a85984933e3b73027e77a7e13bdf9
marjanhasan <moonmarjanhasan@gmail.com>
2024-12-04 00:27:25 +0600

    adding form and slug values done

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, LogoutBtn, Logo };
+export { Header, Footer, Container, LogoutBtn, Logo, RTE };
